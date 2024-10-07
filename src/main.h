cad83b6a36369fa29929b4b9d3d01a7674e15be6
Marina Ring <mring@g.hmc.edu>
2024-10-07 16:12:47 -0700

    Starter files for the lab

---
+++ src/main.h
@@ -0,0 +1,21 @@
+// main.h
+// Josh Brake
+// jbrake@hmc.edu
+// 10/31/22 
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "STM32L432KC.h"
+#include <stm32l432xx.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Custom defines
+///////////////////////////////////////////////////////////////////////////////
+
+#define LED_PIN PA5
+#define ENCODER_A PA4
+#define ENCODER_B PA6
+#define DELAY_TIM TIM2
+
+#endif // MAIN_H
